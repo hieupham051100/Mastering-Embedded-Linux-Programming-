e15e09f0d8a3d3d967e82e6f2568527a5930a570
hieu_pham051100 <phamtrunghieums10@gmail.com>
2025-07-18 19:47:12 +0700

    chapter2 how to create and use lib.a and lib.so

---
+++ target_program/hello-target/hello_target.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void hello_target(){	
+	printf("hello target, from host \n");
+
+}
---
+++ target_program/test_function/test_function.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "test_function.h"
+
+int main(){
+	printf("hello target, form host  i'm main \n ");
+	hello_target();	
+	return 0;
+}
